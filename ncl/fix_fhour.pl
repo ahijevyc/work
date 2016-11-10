1849bbf4a14718b58d36cf9013618ab78c7ac40e
ahijevyc <ahijevyc@ucar.edu>
2016-11-10 14:24:35 -0700

    just adding to git.

---
+++ ncl/fix_fhour.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -wn 
+use strict;
+
+print "load \"/glade/p/work/ahijevyc/ncl/xtime.ncl\"\n" if $. == 6;
+next if /fhour = 0/;
+s/\+fhour\+/+fhour_xtime(ff,f)+/;
+# change do loop increment from 2 to 1
+s/do nf = 0, nfiles-1, *2/do nf = 0, nfiles-1/;
+s/do nf = 0, nfiles-1,1/do nf = 0, nfiles-1/;
+next if /fhour = fhour/;
+print;
+
+
