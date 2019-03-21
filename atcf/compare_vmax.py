0d86b0ff01605afb54a7c958199b2cdb0ea5794d
ahijevyc <ahijevyc@ucar.edu>
2019-03-21 12:43:00 -0600

    input files in command line. nicer labels

--- atcf/compare_vmax.py
+++ atcf/compare_vmax.py
@@ -1,50 +1,64 @@
 import atcf
 import pdb
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import datetime
+
+# provide list of filenames on command line. These, along with their _origmeshTrue counterparts, will be processed.
 
 
 parser = argparse.ArgumentParser(description='scatter plot of vmax from ECMWF TIGGE and official adeck')
 parser.add_argument('-d','--debug', action="store_true", help='print debug messages')
+parser.add_argument('--gs', type=float, default=0.5, help='TIGGE data interpolated to this grid spacing in degrees')
+parser.add_argument('input_files', type=str, nargs='+', help="input files without _origmeshTrue suffix")
 args = parser.parse_args()
 debug = args.debug
+gs    = args.gs
 
 # Run in /glade/work/ahijevyc/atcf/Irma2017 directory
 
 fig, ax = plt.subplots()
-# Isotropic axes
-plt.axis('equal')
-ax.margins(0)
-for i,date in enumerate(["2017090512", "2017090600", "2017090612", "2017090700", "2017090712", "2017090800", "2017090812", "2017090900", "2017090912"]):
-    x = atcf.read(date+".dat")
+dates = []
+for input_file in args.input_files: 
+    x = atcf.read(input_file)
     # The adeck has 2 more models than origmeshTrue: ECMF and EE00.
     x = x[x.model != 'EE00']
     x = x[x.model != 'ECMF']
     # Read vmax/mslp from original mesh
-    y = atcf.read(date+".dat_origmeshTrue",debug=debug)
+    y = atcf.read(input_file+"_origmeshTrue",debug=debug)
     y = y[y.rad <= 34]
     # match lines from original adeck and origmeshTrue.
     z = pd.merge(x, y, on=['model','fhr']) 
+    # Get first initialization time in yyyymmddhh formatted string.
+    date = x.initial_time.dt.strftime('%Y%m%d%H').iloc[0]
+    dates.append(date)
     sc = ax.scatter(z.vmax_x, z.vmax_y, alpha=0.05,label=date, s=1.8, marker="s", color="blue")
 
 
-xlim = ax.get_xlim()
+lim = np.array([0, 150])
 # 1:1 or 100% line
-oo = ax.plot(xlim,xlim,"k--",label="100% of adeck",zorder=0, linewidth=0.8)
+oo = ax.plot(lim,lim,"k--",label="100% of adeck",zorder=0, linewidth=0.8)
 # 80% underestimate line
-oo = ax.plot(xlim,(xlim[0], xlim[1]*0.8),"k", linestyle='dotted',label="80% of adeck",zorder=0,linewidth=0.5)
+oo = ax.plot(lim,lim*0.8,"k", linestyle='dotted',label="80% of adeck",zorder=0,linewidth=0.5)
 # 50% underestimate line
-oo = ax.plot(xlim,(xlim[0], xlim[1]*0.5),"k", linestyle='dotted',label="50% of adeck",zorder=0,linewidth=0.3)
+oo = ax.plot(lim,lim*0.5,"k", linestyle='dotted',label="50% of adeck",zorder=0,linewidth=0.3)
 cats = plt.axvline(34,label='TC strength',zorder=0,alpha=0.5)
 l = ax.legend(fontsize='7')
 cats = plt.axhline(34,zorder=0, alpha=0.5)
 ax.set_xlabel("Vmax (kts) from adeck (native resolution ~18km)")
-ax.set_ylabel("Vmax (kts) from TIGGE (0.5deg)")
-ax.set_ylim(top=140)
+ax.set_ylabel("Vmax (kts) from TIGGE")
+ax.set_xlim(lim)
+ax.set_ylim(lim)
 
-plt.title("Effect of interpolating wind from 18 to 50km for Irma")
-ofile = "compare_vmax.png"
+plt.title("Vmax on native ECMWF mesh vs. "+str(gs)+" deg lat-lon grid")
+date_range = max(dates)
+if min(dates) != max(dates):
+    date_range = min(dates)+"-"+max(dates)
+ofile = "compare_vmax."+str(gs)+"."+date_range+".png"
+string = "\ncreated "+str(datetime.datetime.now(tz=None)).split('.')[0]
+plt.annotate(s=string, xy=(20,2), xycoords='figure pixels', fontsize=5)
 print("savefig", ofile)
 plt.savefig(ofile, dpi=200)
 ax.clear()
